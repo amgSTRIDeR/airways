b3adbd3476f812cddc19a96064b77670a9fa4f87
Pavel Arabei <Ptashkaaaaaa@gmail.com>
2023-04-18 19:10:43 +0200

    feat: add paths

--- airways/src/app/app-routing.module.ts
+++ airways/src/app/app-routing.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { ErrorPageComponent } from './core/pages/error-page/error-page.component';
+import { ErrorPageComponent } from '@core/pages/error-page/error-page.component';
 
 const routes: Routes = [
   {
