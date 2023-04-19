880b20f54bc4287a33417e1c8f43a44cd814d53a
Pavel Arabei <Ptashkaaaaaa@gmail.com>
2023-04-19 02:23:41 +0200

    feat: Passengers page view 70%

--- airways/src/app/booking-page/booking-page.module.ts
+++ airways/src/app/booking-page/booking-page.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
 import { FlightsComponent } from '@booking/pages/flights/flights.component';
 import { BookingPageRoutingModule } from '@booking/booking-page-routing.module';
 import { BookingPageComponent } from '@booking/booking-page.component';
 import { PassengersComponent } from '@booking/pages/passengers/passengers.component';
 import { ReviewComponent } from '@booking/pages/review/review.component';
+import { CommonModule } from '@angular/common';
+import { SharedModule } from '@shared/shared.module';
 
 @NgModule({
   declarations: [
@@ -13,6 +14,6 @@ import { ReviewComponent } from '@booking/pages/review/review.component';
     PassengersComponent,
     ReviewComponent,
   ],
-  imports: [CommonModule, BookingPageRoutingModule],
+  imports: [CommonModule, BookingPageRoutingModule, SharedModule],
 })
 export class BookingPageModule {}
