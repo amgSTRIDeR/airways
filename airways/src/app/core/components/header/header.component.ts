ce5b8e7ee7cbc639d1493761f97d3676ca9de92f
Aleksei Ovchinnikov <amgstrider@gmail.com>
2023-04-21 08:53:27 +0400

    feat: hamburger menu

--- airways/src/app/core/components/header/header.component.ts
+++ airways/src/app/core/components/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 import { Router } from '@angular/router';
 
 @Component({
@@ -8,10 +8,21 @@ import { Router } from '@angular/router';
 })
 export class HeaderComponent {
   public showBookWindow = false;
+  public isUserSignIn = false;
+  public isHamburgerMenuActive = false;
   public selectedDateFormat = 'MM/DD/YYYY';
   public selectedCurrency = 'EUR';
+  windowWidth: number = window.innerWidth;
 
   constructor(private router: Router) {}
+
+  @HostListener('window:resize')
+  onWindowResize() {
+    this.windowWidth = window.innerWidth;
+    if (this.windowWidth >= 750) {
+      this.isHamburgerMenuActive = false;
+    }
+  }
 
   toMainPage() {
     this.router.navigate(['/']);
