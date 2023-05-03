9fac9aff0779955fb36c769d8059a5ed9b6ce784
Aleksei Ovchinnikov <amgstrider@gmail.com>
2023-05-03 11:07:04 +0400

    feat: connect currency and dateType select to redux

--- airways/src/app/core/components/header/header.component.ts
+++ airways/src/app/core/components/header/header.component.ts
@@ -1,22 +1,30 @@
-import { Component, HostListener } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 import { NavigationEnd, Router } from '@angular/router';
+import { Store, select } from '@ngrx/store';
+import { SettingsActions } from '@redux/actions/settings.actions';
+import { CurrencyType, DateType } from '@redux/models/settings.models';
+import { SettingsState } from '@redux/models/state.models';
+import { SettingsSelectors } from '@redux/selectors/settings.selectors';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss'],
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
   public showBookWindow = false;
   public isBookingPage = false;
   public isMainPage = false;
   public isUserSignIn = false;
   public isHamburgerMenuActive = false;
-  public selectedDateFormat = 'MM/DD/YYYY';
-  public selectedCurrency = 'EUR';
+  public selectedDateFormat: DateType = 'MM/DD/YYYY';
+  public selectedCurrency: CurrencyType = 'USA';
+  public currency$!: Observable<CurrencyType>;
+  public dateType$!: Observable<DateType>;
   windowWidth: number = window.innerWidth;
 
-  constructor(private router: Router) {
+  constructor(private router: Router, private store: Store<SettingsState>) {
     this.router.events.subscribe((event) => {
       if (event instanceof NavigationEnd) {
         this.isBookingPage =
@@ -25,6 +33,28 @@ export class HeaderComponent {
         this.isMainPage = event.url.startsWith('/main');
       }
     });
+  }
+
+  ngOnInit() {
+    this.currency$ = this.store.pipe(
+      select(SettingsSelectors.CurrencySelector)
+    );
+    this.currency$.subscribe((currency) => (this.selectedCurrency = currency));
+
+    this.dateType$ = this.store.pipe(
+      select(SettingsSelectors.DateTypeSelector)
+    );
+    this.dateType$.subscribe(
+      (dateType) => (this.selectedDateFormat = dateType)
+    );
+  }
+
+  changeCurrency(currency: CurrencyType) {
+    this.store.dispatch(SettingsActions.selectCurrency({ currency }));
+  }
+
+  changeDateType(dateType: DateType) {
+    this.store.dispatch(SettingsActions.selectDateType({ dateType }));
   }
 
   @HostListener('window:resize')
