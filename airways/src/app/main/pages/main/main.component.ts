c5e253c8fdb3905521ff55dc193fe7dc897f5e00
Aleksei Ovchinnikov <amgstrider@gmail.com>
2023-05-05 17:41:44 +0400

    refactor: main component remarks from teammate

--- airways/src/app/main/pages/main/main.component.ts
+++ airways/src/app/main/pages/main/main.component.ts
@@ -1,49 +1,38 @@
-import { Component, OnInit } from '@angular/core';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { Airport } from '@shared/interfaces/airport.interface';
+import { Component } from '@angular/core';
 import { SettingsState } from '@redux/models/state.models';
 import { SettingsSelectors } from '@redux/selectors/settings.selectors';
 import { Store, select } from '@ngrx/store';
+import { DataService } from '@core/services/data.service';
+import { AirportsRes } from '@redux/models/main-page.models';
 
 @Component({
   selector: 'app-main',
   templateUrl: './main.component.html',
   styleUrls: ['./main.component.scss'],
 })
-export class MainComponent implements OnInit {
-  airports: Airport[] = [];
-  public originAirport: Airport = { key: '', name: '', country: '', city: '' };
-  public destinationAirport: Airport = {
-    key: '',
-    name: '',
-    country: '',
-    city: '',
-  };
+export class MainComponent {
+  airports: AirportsRes[] = [];
+  public originAirport: AirportsRes | null = null;
+  public destinationAirport: AirportsRes | null = null;
+
   dateType$ = this.store.pipe(select(SettingsSelectors.DateTypeSelector));
   public tripType = 'roundTrip';
   public departureDate!: Date;
   public returnDate!: Date;
 
-  constructor(private http: HttpClient, private store: Store<SettingsState>) {}
-
-  ngOnInit(): void {
-    const httpOptions = {
-      headers: new HttpHeaders({
-        'Content-Type': 'application/json',
-        Accept: 'application/json',
-      }),
-    };
-
-    this.http
-      .get<Airport[]>('/api/search/airport', httpOptions)
-      .subscribe((data) => {
-        this.airports = data;
-      });
+  constructor(
+    private store: Store<SettingsState>,
+    private readonly dataService: DataService
+  ) {
+    this.dataService.getAirports().subscribe((data) => {
+      this.airports = data;
+    });
   }
 
   switchLocations() {
-    const temp = this.originAirport;
-    this.originAirport = this.destinationAirport;
-    this.destinationAirport = temp;
+    [this.originAirport, this.destinationAirport] = [
+      this.destinationAirport,
+      this.originAirport,
+    ];
   }
 }
--- airways/src/app/main/pipes/airports-filter.pipe.ts
+++ airways/src/app/main/pipes/airports-filter.pipe.ts
@@ -1,14 +1,19 @@
 import { Pipe, PipeTransform } from '@angular/core';
-import { Airport } from '@shared/interfaces/airport.interface';
+import { AirportsRes } from '@redux/models/main-page.models';
 
 @Pipe({
   name: 'airportsFilter',
 })
 export class AirportsFilterPipe implements PipeTransform {
-  transform(airports: Airport[], selectedAirport: Airport): Airport[] {
+  transform(
+    airports: AirportsRes[] | null,
+    selectedAirport: AirportsRes | null
+  ): AirportsRes[] {
     if (!selectedAirport) {
-      return airports;
+      return airports || [];
+    } else if (airports) {
+      return airports.filter((airport) => airport.key !== selectedAirport.key);
     }
-    return airports.filter((airport) => airport.key !== selectedAirport.key);
+    return [];
   }
 }
