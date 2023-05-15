90e25b021f4eca2b79704128e4295fdeb1d31c61
Aleksei Ovchinnikov <amgstrider@gmail.com>
2023-05-15 21:16:37 +0400

    feat: passengers form markup

--- airways/src/app/shared/components/passengers-form/passengers-form.component.ts
+++ airways/src/app/shared/components/passengers-form/passengers-form.component.ts
@@ -1,4 +1,11 @@
-import { Component, OnDestroy } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  HostListener,
+  OnDestroy,
+  Renderer2,
+  ViewChild,
+} from '@angular/core';
 import { Store } from '@ngrx/store';
 import { MainPageActions } from '@redux/actions/main-page.actions';
 import { PassengersCount } from '@redux/models/main-page.models';
@@ -12,16 +19,23 @@ import { Subscription } from 'rxjs';
   styleUrls: ['./passengers-form.component.scss'],
 })
 export class PassengersFormComponent implements OnDestroy {
+  @ViewChild('passengersForm') passengersFormRef!: ElementRef;
+  @ViewChild('passengersControl') passengersControlRef!: ElementRef;
+
   public passengers = {
     adults: 1,
     children: 0,
     infants: 0,
   };
+  public isControlVisible = false;
 
   passengersCount$ = this.store.select(MainPageSelectors.PassengersCount);
   passengersSubscription!: Subscription;
 
-  constructor(private store: Store<SettingsState>) {
+  constructor(
+    private store: Store<SettingsState>,
+    private renderer: Renderer2
+  ) {
     this.passengersSubscription = this.passengersCount$.subscribe(
       (passengers: PassengersCount | null): void => {
         if (passengers !== null) {
@@ -33,6 +47,19 @@ export class PassengersFormComponent implements OnDestroy {
         }
       }
     );
+  }
+
+  @HostListener('document:click', ['$event.target'])
+  onClick(target: unknown) {
+    const clickedOnForm =
+      this.passengersFormRef?.nativeElement.contains(target);
+
+    const clickedInsideControl =
+      this.passengersControlRef?.nativeElement.contains(target);
+
+    if (!clickedInsideControl && !clickedOnForm) {
+      this.isControlVisible = false;
+    }
   }
 
   ngOnDestroy(): void {
