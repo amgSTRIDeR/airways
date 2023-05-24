360821d4a1541089190c99320d9afad783bf15ee
Pavel Aleshkin <vinci.aleshkin@gmail.com>
2023-05-24 04:15:15 +0300

    add flights page

---
+++ airways/src/app/booking-page/components/alternative-flight-card/alternative-flight-card.component.ts
@@ -0,0 +1,20 @@
+import { Component, Input, OnChanges } from '@angular/core';
+import { IAlternativeFlight, IPrice } from '@redux/models/main-page.models';
+
+@Component({
+  selector: 'app-alternative-flight-card',
+  templateUrl: './alternative-flight-card.component.html',
+  styleUrls: ['./alternative-flight-card.component.scss']
+})
+export class AlternativeFlightCardComponent implements OnChanges {
+  @Input() flightInfo!: IAlternativeFlight;
+  @Input() chosenCurrency: string = 'EUR';
+
+  public price?: number | null = 0;
+
+  constructor() {};
+
+  ngOnChanges() {
+    this.price = !!this.flightInfo.price ? this.flightInfo.price[this.chosenCurrency.toLowerCase() as keyof IPrice] : null;
+  }
+}
