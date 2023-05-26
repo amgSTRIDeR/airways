0fe73a82bddd585744866829e1f70ca11bb6c86c
Pavel Aleshkin <vinci.aleshkin@gmail.com>
2023-05-26 23:16:44 +0300

    feat: add slider for alternative flights on flight page

--- airways/src/app/booking-page/components/booking-flight-details/booking-flight-details.component.ts
+++ airways/src/app/booking-page/components/booking-flight-details/booking-flight-details.component.ts
@@ -1,6 +1,6 @@
 import { Component, Input, OnInit, OnChanges } from '@angular/core';
 import { Store } from '@ngrx/store';
-import { FlightsRes, IAlternativeFlight, IOtherFlights, IPrice } from '@redux/models/main-page.models';
+import { FlightRes, FlightsRes, IAlternativeFlight, IOtherFlights, IPrice } from '@redux/models/main-page.models';
 import { SettingsSelectors } from '@redux/selectors/settings.selectors';
 import { Observable } from 'rxjs';
 
@@ -17,6 +17,8 @@ export class BookingFlightDetailsComponent implements OnInit, OnChanges {
   public chosenCurrency: string = 'EUR';
   public price?: number = 0;
   public alternativeFlights: IAlternativeFlight[] = [];
+  public activeIndex: number = 0;
+  public activeFlight?: FlightsRes | FlightRes;
 
   private currencyInfo$: Observable<string> = this.store.select(
     SettingsSelectors.CurrencySelector
@@ -27,13 +29,39 @@ export class BookingFlightDetailsComponent implements OnInit, OnChanges {
   ngOnInit(): void {
     this.currencyInfo$.subscribe((storeCurrency) => {
       this.chosenCurrency = storeCurrency;
-      this.price = this.flightInfo?.price[this.chosenCurrency.toLowerCase() as keyof IPrice] || 0;
+      this.updateActiveFlight();
     });
   }
 
   ngOnChanges(): void {
     this.flightTitle = `${this.flightInfo?.form.city} to ${this.flightInfo?.to.city}`;
+    this.updateActiveFlight();
     this.getAlternativeFlightsArray();
+  }
+
+  increaseIndex(): void {
+    if (this.activeIndex < 5) {
+      this.activeIndex++;
+      this.updateActiveFlight();
+    }
+    this.getNoTicketsMessage();
+  }
+
+  decreaseIndex(): void {
+    if (this.activeIndex > -5) {
+      this.activeIndex--;
+      this.updateActiveFlight();
+    }
+    this.getNoTicketsMessage();
+  }
+
+  updateActiveFlight(): void {
+    this.activeFlight = this.activeIndex === 0 ? this.flightInfo : this.flightInfo?.otherFlights[this.activeIndex.toString() as keyof IOtherFlights];
+    this.price = this.activeFlight?.price[this.chosenCurrency.toLowerCase() as keyof IPrice] || 0;
+  }
+
+  showAlternativeFlight(flightIndex: number): boolean {
+    return Math.abs(this.activeIndex - flightIndex) <= 2 || this.activeIndex >= 3 && flightIndex > 0 || this.activeIndex <= -3 && flightIndex < 0;
   }
 
   getAlternativeFlightsArray(): void {
@@ -62,6 +90,12 @@ export class BookingFlightDetailsComponent implements OnInit, OnChanges {
         seats: alternativeFlightInfo?.seats,
       });
     }
+  }
+
+  getNoTicketsMessage(): string {
+    const currentDate = new Date().getTime();
+    const activeFlightDate = new Date(this.addDays(new Date(this.flightInfo!.takeoffDate), this.activeIndex)).getTime();
+    return activeFlightDate < currentDate ? 'Билеты в прошлом недоступны к продаже' : 'Билет на выбранную дату отсутствует';
   }
 
   addDays(date: Date, days: number): string {
