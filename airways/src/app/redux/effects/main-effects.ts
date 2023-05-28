b59c25d9bf34a5c6163d6abd26b11c43c39f4893
Aleksei Ovchinnikov <amgstrider@gmail.com>
2023-05-28 09:11:14 +0400

    fix: form errors after fligths search

--- airways/src/app/redux/effects/main-effects.ts
+++ airways/src/app/redux/effects/main-effects.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
 import { DataService } from '@core/services/data.service';
 import { Actions, createEffect, ofType } from '@ngrx/effects';
 import { BookingActions } from '@redux/actions/booking-page.actions';
@@ -7,7 +8,11 @@ import { catchError, map, of, switchMap } from 'rxjs';
 
 @Injectable()
 export class MainEffects {
-  constructor(private actions$: Actions, private dataService: DataService) {}
+  constructor(
+    private actions$: Actions,
+    private dataService: DataService,
+    private router: Router
+  ) {}
 
   loadAirports$ = createEffect(() =>
     this.actions$.pipe(
@@ -35,11 +40,14 @@ export class MainEffects {
             action.returnDate
           )
           .pipe(
-            map((flights) =>
-              BookingActions.LoadAvailableFlightsSuccess({
+            map((flights) => {
+              if (this.router.url !== '/booking-page') {
+                this.router.navigate(['/booking-page']);
+              };
+              return BookingActions.LoadAvailableFlightsSuccess({
                 availableFlights: flights,
-              })
-            ),
+              });
+            }),
             catchError((error) =>
               of(MainPageActions.LoadAvailableFlightsError({ error }))
             )
